24f818a5ffdd9b8361c8b20fcbb3966da4575308
Princess-Morris <okpataprincessmorris3@gmail.com>
2024-07-09 18:34:35 +0100

    Add feedbacklist, feedbackItem, feedbackstat, data folder, and shared style component

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,37 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
+
+    },
+
+    {
+        id: 2,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
+
+    },
+
+    {
+        id: 3,
+        rating: 8,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
+
+    },
+    {
+        id: 4,
+        rating: 3,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
+
+    },
+
+    {
+        id: 5,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt'
+
+    }
+]
+
+export default FeedbackData 
