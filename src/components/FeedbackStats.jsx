45953a15f985aaa007be734e0001b22147ab3495
Princess-Morris <okpataprincessmorris3@gmail.com>
2024-07-09 19:35:37 +0100

    add a function to calculate feedback avearge rating

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,12 +1,33 @@
-function FeedbackStats({feedback}) {
-// ca
+import PropTypes from 'prop-types';
 
-  return (
+function FeedbackStats({feedback}) {
+// Calculate ratings avg
+
+let average = feedback.reduce((acc, cur) => {
+    return acc + cur.rating
+}, 0) / feedback.length
+
+average = average.toFixed(1).replace(/[.,]0$/, '')
+
+    // let average = 0
+
+    // for (let i = 0; i < feedback.length; i++){
+    //     average = average + feedback[i].rating
+    // }
+
+    // average = average / feedback.length
+
+
+  return ( 
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: 10</h4>
+      <h4>{isNaN(average) ? 0 : average}</h4>
     </div>
   );
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats;
