a835a103f7d9758a1651f793f86f2908477d5a44
Princess-Morris <okpataprincessmorris3@gmail.com>
2024-07-10 15:31:57 +0100

    add framer-motion for animation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from 'uuid'
 import { useState } from 'react';
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData';
@@ -9,7 +10,9 @@ function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
   
   const addFeedback = (newFeedback) => {
-      console.log(newFeedback)
+    newFeedback.id = uuidv4()
+      // console.log(newFeedback)
+      setFeedback([newFeedback, ...feedback])
   }
 
   const deleteFeedback = (id) => {
